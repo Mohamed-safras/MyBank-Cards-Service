23de8144112474cba3feb6b62fa24df0e5a168cc
Mohamed Safras <awsafras@gmail.com>
2024-10-22 09:48:11 +0530

    card service has been initialized

---
+++ src/test/java/org/acentura/cards/CartApplicationTests.java
@@ -0,0 +1,13 @@
+package org.acentura.cards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
