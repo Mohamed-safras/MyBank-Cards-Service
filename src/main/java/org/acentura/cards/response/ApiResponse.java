e7ec3d5a3fe37e8795c3acbd31d6b85a0caa2b71
Mohamed Safras <mohamed.safras@acentura.com>
2024-10-31 15:44:10 +0530

    Api response has been added

--- src/main/java/org/acentura/cards/response/ApiResponse.java
+++ src/main/java/org/acentura/cards/response/ApiResponse.java
@@ -1,8 +1,10 @@
-package org.acentura.cards.dto;
+package org.acentura.cards.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.lang.Nullable;
 
 @Schema(
         name = "Response",
@@ -10,7 +12,8 @@ import lombok.Data;
 )
 @Data
 @AllArgsConstructor
-public class ResponseDto {
+@NoArgsConstructor
+public class ApiResponse<T> {
 
     @Schema(
             description = "Status code in the response"
@@ -22,4 +25,16 @@ public class ResponseDto {
     )
     private String statusMsg;
 
+
+    @Schema(
+            name = "data",
+            description = ""
+    )
+    @Nullable
+    private T data;
+
+    public ApiResponse(String status201, String message201) {
+        this.statusCode = status201;
+        this.statusMsg = message201;
+    }
 }
